22b954b4b43a2e294de8b77cdd3d89b0830ee2eb
MikeWKI <Michaela@WichitaKenworth.com>
2025-06-27 15:17:09 -0500

    fix: rename component file to PascalCase and update import

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TechnicianTracker from './components/techniciantracker';
+
+function App() {
+  return (
+    <div className="App">
+      <TechnicianTracker />
+    </div>
+  );
+}
+
+export default App;
